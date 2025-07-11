a56d7cc9f4af96b06cb5cbcabcdbb5980f1a246c
Android Studio <devandroid@ort>
2025-07-11 10:32:32 -0300

    get

--- index.js
+++ index.js
@@ -12,13 +12,24 @@ let eventosArray = [];
 
 
 app.use(cors());
-
+/*
 app.get('/api/event', async (req, res) => {
     const client = new Client(config)
     try {
         console.log("Entro al try")
         await client.connect()
-        let sql = `SELECT * FROM "events"`
+        let sql = ` SELECT 
+        e.id,
+        e.name,
+        e.description,
+        e.start_date,
+        e.duration_in_minutes,
+        e.price,
+        e.enabled_for_enrollment,
+        e.max_assistance,
+        l.name AS location_name
+        FROM "events" e
+        INNER JOIN "locations" l ON e.id_event_location = l.id`
         let eventos = await client.query(sql);
         eventosArray = eventos;
         res.status(200).send(eventos.rows);
@@ -35,7 +46,74 @@ app.get('/api/event', async (req, res) => {
     }
 
 });
+*/
 
+
+
+app.get('/api/event', async (req, res) => {
+    const client = new Client(config);
+    const { name, startdate, tag } = req.query;
+
+    try {
+        console.log("Entro al try");
+        await client.connect();
+
+        let sql = `
+            SELECT 
+                e.id,
+                e.name,
+                e.description,
+                e.start_date,
+                e.duration_in_minutes,
+                e.price,
+                e.enabled_for_enrollment,
+                e.max_assistance,
+                e.id_event_category,
+                l.id AS location_id,
+                l.name AS location_name,
+                l.latitude,
+                l.longitude
+            FROM events e
+            INNER JOIN locations l ON e.id_event_location = l.id
+            WHERE (e.name ILIKE $1) 
+            AND (e.start_date ILIKE $2)
+            AND (e.id_event_category ILIKE $3)
+        `;
+
+        /*
+         WHERE 
+                ($1 IS NULL OR e.name ILIKE CONCAT('%', $1, '%') )
+            AND
+                ($2 IS NULL OR  e.start_date = $2)
+            AND
+                ($3 IS NULL OR e.id_event_category::text = $3)
+        `;
+        */
+        
+        let values = [
+            '%'+name+'%',
+            '%'+startdate+'%',
+            '%'+tag+'%'
+        ];
+        console.log('sql', sql);
+        console.log('values', values);
+        let eventos = await client.query(sql, values);
+
+        eventosArray = eventos;
+        res.status(200).send(eventos.rows);
+    }
+
+    catch (error) {
+        console.log('mich')
+        res.status(500).send(error);
+        console.log(error);
+    }
+
+    finally {
+        await client.end();
+    }
+});
+ 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
